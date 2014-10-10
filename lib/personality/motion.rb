4e7ae675cf85db4a4f5a6897c9e857ba162e6966
Neil Gupta <neil@tabuleapp.com>
2014-10-10 05:24:01 -0500

    motion personality skeleton

---
+++ lib/personality/motion.rb
@@ -0,0 +1,24 @@
+module Personality
+  class Motion < Base
+    def generate_reaction(motor, sensor_type, data)
+      return unless sensor_type == 'motion' || sensor_type == 'sound'
+
+      if sensor_type == 'motion'
+
+        # do something
+
+        # if it's been more than 10 min since last motion, say hello!
+        unless Input.joins(sensor: [brain: :motor]).where("motors.id = ? and sensors.type = 'motion' and age(inputs.created_at) < interval '10 minutes'", motor.id).exists?
+          return 'ACTION - HELLO'
+        end
+
+      elsif sensor_type == 'sound'
+
+        # do something else
+
+        # if there is a lot of noise
+
+      end
+    end
+  end
+end
