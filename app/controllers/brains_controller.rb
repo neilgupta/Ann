3af61d380f4b98c22901f4f6c8d624a9918e3959
Neil Gupta <neil@tabuleapp.com>
2014-10-11 11:45:22 -0500

    Compare addresses all lowercase

--- app/controllers/brains_controller.rb
+++ app/controllers/brains_controller.rb
@@ -2,15 +2,15 @@ class BrainsController < ApplicationController
 
   # Streaming set of instructions for CNS to disseminate
   def instructions
-    brain = Brain.find_by_address(params[:address])
+    brain = Brain.where("lower(address) = ?", params[:address].downcase).first
     render json: brain.fetch_instructions, root: false
   end
 
   # Endpoint for pushing sensory data to brain
   def senses
-    brain = Brain.find_by_address(params[:address])
+    brain = Brain.where("lower(address) = ?", params[:address].downcase).first
 
-    sensor = Sensor.where("address = ? and brain_id = ?", params[:sensor_addr], brain.id).first
+    sensor = Sensor.where("lower(address) = ? and brain_id = ?", params[:sensor_addr].downcase, brain.id).first
     sensor.save_data(params[:payload]) if sensor && params[:payload]
 
     head :ok # return empty 200 response
