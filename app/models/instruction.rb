7c442fed438e7669017398038779f7991c66d2fb
Neil Gupta <neil@tabuleapp.com>
2014-10-11 07:45:46 -0500

    Update instruction mappings

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -12,23 +12,25 @@ class Instruction < ActiveRecord::Base
     when 'ACTION - ROTATE'    then 'c' # Rotate once
     when 'ACTION - NOD'       then 'd' # Nod twice
     when 'ACTION - STOP'      then 'e' # Stand still and turn off lights
+    when 'ACTION - CHIME'     then 't' # Play an hourly chime given the hour number
+    when 'ACTION - JOKE'      then 'p' # Play joke sound
     when 'COLOR - GREEN'      then 'f' # Turn all lights green
     when 'COLOR - BLUE'       then 'g' # Turn all lights blue
-    when 'COLOR - RED'        then 'h' # Turn all lights red
-    when 'WEATHER - SUNNY'    then ‘i’ # alternating blue and green ropes, play sunny sound
-    when 'WEATHER - RAINY'    then ‘j’ # alternating blue and off, play rainy sound
-    when 'WEATHER - CLOUDY'   then ‘k’ # 20% blue, 80% off
-    when 'WEATHER - DARK'     then ‘l’ # all off, play night sound
-    when 'WEATHER - SNOWY'    then ‘m’ # single red rope randomly moving around
-    when 'WEATHER - SUNRISE'  then ’n’ # alternating blue and red
-    when 'WEATHER - COLD'     then ‘o’ # single blue rope randomly moving around
-    when 'EMOTION - HAPPY'    then 'p' # dance?, play laugh sound
-    when 'EMOTION - SAD'      then 'q' # Turn lights blue and yellow, rotate slowly
-    when 'EMOTION - STARTLED' then ‘r’ # Flash green lights, play shrug sound
-    when 'EMOTION - ANGRY'    then ’s’ # Turn red for a second, play growl sound
-    when 'EMOTION - CONFUSED' then ’t’ # Use all 3 colors in a random pattern, play surprised sound
-    when 'EMOTION - SOCIAL'   then ‘u’ # nod?
-    when 'EMOTION - HUNGRY'   then ‘v’ # turn green for a second, play growl sound
+    when 'COLOR - RED'        then 'k' # Turn all lights red
+    when 'WEATHER - SUNNY'    then 'i' # alternating blue and green ropes, play sunny sound
+    when 'WEATHER - RAINY'    then 'o' # alternating blue and off, play rainy sound
+    when 'WEATHER - CLOUDY'   then 'j' # 20% blue, 80% off
+    when 'WEATHER - DARK'     then 'l' # all off, play night sound
+    when 'WEATHER - SNOWY'    then 'm' # single red rope randomly moving around
+    when 'WEATHER - SUNRISE'  then 'n' # alternating blue and red
+    when 'WEATHER - COLD'     then 'o' # single blue rope randomly moving around
+    when 'EMOTION - HAPPY'    then 'f' # dance?, play laugh sound
+    when 'EMOTION - SAD'      then 'r' # Turn lights blue and yellow, rotate slowly
+    when 'EMOTION - STARTLED' then 's' # Flash green lights, play shrug sound
+    when 'EMOTION - ANGRY'    then 'h' # Turn red for a second, play growl sound
+    when 'EMOTION - CONFUSED' then 'q' # Use all 3 colors in a random pattern, play surprised sound
+    when 'EMOTION - SOCIAL'   then 'd' # nod?
+    when 'EMOTION - HUNGRY'   then 'g' # turn green for a second, play growl sound
     else action
     end
   end
