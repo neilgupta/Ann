fe10dc75b2a73aca13b9cffcfb5ef09199fc0fe5
Neil Gupta <neil@tabuleapp.com>
2014-11-02 03:41:24 -0600

    Updated unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,28 @@
-worker_processes 2 # amount of unicorn workers to spin up
-timeout 30         # restarts workers that hang for 30 seconds
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 2)
+timeout Integer(ENV['WEB_TIMEOUT'] || 90)
+preload_app true
+
+before_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  if defined?(ActiveRecord::Base)
+    config = ActiveRecord::Base.configurations[Rails.env]
+    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+    config['pool'] = ENV['DB_POOL'] || 2
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
